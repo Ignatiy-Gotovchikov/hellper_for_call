652cdabec6116672eae9e8b4c4e108c5d3508491
ignatiygotovchikov <ignatsergeevich10@gmail.com>
2023-07-09 20:14:17 +0300

    create users api/serializers/views

--- users/serializers/api/users.py
+++ users/serializers/api/users.py
@@ -6,6 +6,8 @@ from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ParseError
 
+from users.serializers.nested.profile import ProfileShortSerializer, ProfileUpdateSerializer
+
 # from users.serializers.nested.profile import ProfileShortSerializer, \
 #     ProfileUpdateSerializer
 
@@ -39,93 +41,102 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def validate_password(self, value):
         validate_password(value)
         return value
-    #
-    # def create(self, validated_data):
-    #     user = User.objects.create_user(**validated_data)
-    #     return user
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
-# class ChangePasswordSerializer(serializers.ModelSerializer):
-#     old_password = serializers.CharField(write_only=True)
-#     new_password = serializers.CharField(write_only=True)
-#
-#     class Meta:
-#         model = User
-#         fields = ('old_password', 'new_password')
-#
-#     def validate(self, attrs):
-#         user = self.instance
-#         old_password = attrs.pop('old_password')
-#         if not user.check_password(old_password):
-#             raise ParseError(
-#                 'Проверьте правильность текущего пароля.'
-#             )
-#         return attrs
-#
-#     def validate_new_password(self, value):
-#         validate_password(value)
-#         return value
-#
-#     def update(self, instance, validated_data):
-#         password = validated_data.pop('new_password')
-#         instance.set_password(password)
-#         instance.save()
-#         return instance
-#
-#
-# class MeSerializer(serializers.ModelSerializer):
-#     profile = ProfileShortSerializer()
-#
-#     class Meta:
-#         model = User
-#         fields = (
-#             'id',
-#             'first_name',
-#             'last_name',
-#             'email',
-#             'phone_number',
-#             'username',
-#             'profile',
-#             'date_joined',
-#         )
-#
-#
-# class MeUpdateSerializer(serializers.ModelSerializer):
-#     profile = ProfileUpdateSerializer()
-#
-#     class Meta:
-#         model = User
-#         fields = (
-#             'id',
-#             'first_name',
-#             'last_name',
-#             'email',
-#             'phone_number',
-#             'username',
-#             'profile',
-#         )
-#
-#     def update(self, instance, validated_data):
-#         # Проверка наличия профиля
-#         profile_data = validated_data.pop('profile') if 'profile' in validated_data else None
-#
-#         with transaction.atomic():
-#             instance = super().update(instance, validated_data)
-#
-#             # # Update профиля
-#             if profile_data:
-#                 self._update_profile(instance.profile, profile_data)
-#
-#         return instance
-#
-#     def _update_profile(self, profile, data):
-#         profile_serializer = ProfileUpdateSerializer(
-#             instance=profile, data=data, partial=True
-#         )
-#         profile_serializer.is_valid(raise_exception=True)
-#         profile_serializer.save()
-#
-#
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password')
+
+    def validate(self, attrs):
+        user = self.instance
+        old_password = attrs.pop('old_password')
+        if not user.check_password(old_password):
+            raise ParseError(
+                'Проверьте правильность текущего пароля.'
+            )
+        return attrs
+
+    def validate_new_password(self, value):
+        validate_password(value)
+        return value
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('new_password')
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+
+
+
+class MeSerializer(serializers.ModelSerializer):
+    profile = ProfileShortSerializer()
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'username',
+            'profile',
+            'date_joined',
+        )
+
+
+class MeUpdateSerializer(serializers.ModelSerializer):
+    profile = ProfileUpdateSerializer()
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'username',
+            'profile',
+        )
+
+    def update(self, instance, validated_data):
+        # Проверка наличия профиля
+        profile_data = validated_data.pop('profile') if 'profile' in validated_data else None
+
+        if profile_data:
+            profile = instance.profile
+            for key, value in profile_data.items():
+                if hasattr(profile, key):
+                    setattr(profile, key, value)
+            profile.save()
+
+        with transaction.atomic():
+            instance = super().update(instance, validated_data)
+
+        # Update профиля
+        if profile_data:
+            self._update_profile(instance.profile, profile_data)
+
+        return instance
+
+    def _update_profile(self, profile, data):
+        profile_serializer = ProfileUpdateSerializer(
+            instance=profile, data=data, partial=True
+        )
+        profile_serializer.is_valid(raise_exception=True)
+        profile_serializer.save()
+
+
 # class UserSearchListSerializer(serializers.ModelSerializer):
 #
 #     class Meta:
